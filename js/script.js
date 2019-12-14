049b29e4d342b80bcb9ab08376154a956654b188
Artem <artem25805@gmail.com>
2019-12-14 17:51:57 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-console.log("test");
+    $("#header").flowtype({ fontRatio: 30 });
+// let f = parseInt($('#footer').css('font-size'))
+// console.log('f - ' + f );
+// let fmax = parseInt($('#footer').css('max-width'))
+// console.log('fmax - ' + fmax );
+// let fo = fmax / f
+// console.log('fo - ' + fo );
 
-console.log("test");
+
+// $(window).resize(function(){
+//     let fwid = parseInt($('#footer').css('width'))
+//     console.log('fwid - ' + fwid );
+//     let fo2 = fwid / fo
+//     console.log('fo2 - ' + fo2 );
+//     $('#footer').css('font-size',fo2 + 'px')
+// });
+
+    // console.log($('#footer').css('font-size'));
+    // document.querySelector("#footer").
