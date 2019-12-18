647c2bb9ecbb7b30b00c16d7637de313b1017c00
Artem <artem25805@gmail.com>
2019-12-18 15:38:40 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-function sgo(){
-    $('.w1210.f16').css('font-size', (parseInt($('.w1210').css('width')) / (1210 / 16)) + 'px') // font-size: 16px
-}
-sgo()
-$(window).resize(function(){
-    sgo()
-})
+import test from 'test';
+test()
+// function sgo(){
+//     $('.w1210.f16').css('font-size', (parseInt($('.w1210').css('width')) / (1210 / 16)) + 'px') // font-size: 16px
+// }
+// sgo()
+// $(window).resize(function(){
+//     sgo()
+// })
+
+
 
