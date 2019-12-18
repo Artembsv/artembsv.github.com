53ada1f767bf96105aa48894ff2deb95514bb87e
Artem <artem25805@gmail.com>
2019-12-18 15:41:36 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import test from 'js/test';
+import test from 'test';
 test()
 // function sgo(){
 //     $('.w1210.f16').css('font-size', (parseInt($('.w1210').css('width')) / (1210 / 16)) + 'px') // font-size: 16px
